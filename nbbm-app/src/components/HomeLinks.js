a4a12cc300adeee2e1229ecc8f3c9067efc0aa29
Jesse Ma <j.masonoma17@gmail.com>
2018-08-17 03:03:32 -0700

    Refactor login & registration button-links into its own component, HomeLinks

---
+++ nbbm-app/src/components/HomeLinks.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './HomeLinks.css';
+
+import { Link } from 'react-router-dom';
+
+class HomeLinks extends Component {
+  render() {
+    return (
+      <div className="HomeLinks">
+        <nav>
+          <button className="homelinks-login homelinks"><Link className="homelinks-inactive" exact to="/login" style={{display: 'block', height: '100%', padding: '12px 0px'}}> Login </Link></button>
+          <button className="homelinks-register homelinks"><Link className="homelinks-inactive" exact to="/register" style={{display: 'block', height: '100%', padding: '12px 0px'}}> Register </Link></button>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default HomeLinks;
