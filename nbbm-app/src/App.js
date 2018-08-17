5758734c9fa2610e113ebe09689e1098875eb368
Jesse Ma <j.masonoma17@gmail.com>
2018-08-17 01:09:30 -0700

    Import core react-routing components to App.js and index.js

--- nbbm-app/src/App.js
+++ nbbm-app/src/App.js
@@ -1,44 +1,16 @@
 import React, { Component } from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import './App.css';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faUser, faEnvelope, faLock);
+import Register from './components/Register';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="app-title"> North Bay Badminton Group </h1>
-        <div className="signup">
-          <form>
-            <div>
-              <label className={['user', 'input-field'].join(' ')} >
-                <input type="text" placeholder="Enter Full Name" autoComplete="disabled" />
-              </label>
-            </div>
-            <div>
-              <label className={['email', 'input-field'].join(' ')}>
-                <input type="text" placeholder="Email Address" autoComplete="disabled" />
-              </label>
-            </div>
-            <div>
-              <label className={['password', 'input-field'].join(' ')}>
-                <FontAwesomeIcon className="password" icon="lock" />
-                <input type="password" placeholder="Password" />
-              </label>
-            </div>
-            <div>
-              <label className={['password', 'input-field'].join(' ')}>
-                <FontAwesomeIcon className="password" icon="lock" />
-                <input type="password" placeholder="Confirm Password" />
-              </label>
-            </div>
-            <input type="submit" value="Register" />
-          </form>
-        </div>
+        <Switch>
+          <Route exact path="/" component={ Register } />
+        </Switch>
       </div>
     );
   }
