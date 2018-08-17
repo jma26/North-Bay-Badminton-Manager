91b0590d8d4c31c060f14ca270ed59742009bed0
Jesse Ma <j.masonoma17@gmail.com>
2018-08-17 03:04:38 -0700

    Add new react <Route/> for Register component in App.js

--- nbbm-app/src/App.js
+++ nbbm-app/src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch } from 'react-router-dom';
 import './App.css';
 
+
+import Home from './components/Home';
 import Register from './components/Register';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={ Register } />
-        </Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/register" component={ Register } />
       </div>
     );
   }
