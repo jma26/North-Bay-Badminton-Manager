57b52d3286c5728b4171b7f4d200091929dfd09f
Jesse Ma <j.masonoma17@gmail.com>
2018-08-18 23:58:40 -0700

    Add form elements and submission, add message state in SocketChat component

--- nbbm-app/frontend/src/components/SocketChat.js
+++ nbbm-app/frontend/src/components/SocketChat.js
@@ -4,13 +4,28 @@ import io from 'socket.io-client';
 class SocketChat extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      'message': '',
+      'messages': []
+    }
     this.socket = io('localhost:8000');
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      'messages': [...this.state.messages, this.state.message]
+    })
   }
 
   render() {
     return (
       <div className="SocketChat">
-
+        <h2> Community Chat </h2>
+        <form onSubmit={(e) => this.handleSubmit(e)} >
+          <input type="text" name="message" value={this.state.message} onChange={(e) => this.setState({message: e.target.value})} placeholder="New Message" />
+          <input type="submit" value="Send" />
+        </form>
       </div>
     )
   }
