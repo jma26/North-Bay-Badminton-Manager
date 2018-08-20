068cf8129c6773788b234bf7e69010a551c10431
Jesse Ma <j.masonoma17@gmail.com>
2018-08-20 16:49:03 -0700

    Update Platform.js with imported Switch, Route component from react-router

--- nbbm-app/frontend/src/components/Platform.js
+++ nbbm-app/frontend/src/components/Platform.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
 import './Platform.css';
 
 import SocketChat from './SocketChat';
@@ -7,7 +8,12 @@ class Platform extends Component {
   render() {
     return (
       <div className="Platform">
-        <SocketChat />
+        <button className="platformlinks-chat platformlinks">
+          <Link to="/platform/chat"> Chatroom </Link>
+        </button>
+        <Switch>
+          <Route path="/platform/chat" component={ SocketChat } />
+        </Switch>
       </div>
     )
   }
