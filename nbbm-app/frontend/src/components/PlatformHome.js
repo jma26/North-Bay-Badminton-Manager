64423e0af8b80dce44784d576486025186a201e4
Jesse Ma <j.masonoma17@gmail.com>
2018-08-20 17:12:50 -0700

    Add a home component for Platform as PlatformHome.js

---
+++ nbbm-app/frontend/src/components/PlatformHome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './PlatformHome.css';
+
+class PlatformHome extends Component {
+  render() {
+    return (
+      <div className="PlatformHome">
+      </div>
+    )
+  }
+}
+
+export default PlatformHome;
