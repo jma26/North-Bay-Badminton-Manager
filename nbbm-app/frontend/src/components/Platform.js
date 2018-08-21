7d4d3ffac97c0ffbd64da825a24a5ee04659ad70
Jesse Ma <j.masonoma17@gmail.com>
2018-08-20 17:14:01 -0700

    Add button links for PlatformHome component, and render PlatformHome by default

--- nbbm-app/frontend/src/components/Platform.js
+++ nbbm-app/frontend/src/components/Platform.js
@@ -3,14 +3,21 @@ import { Link, Switch, Route } from 'react-router-dom';
 import './Platform.css';
 
 import SocketChat from './SocketChat';
+import PlatformHome from './PlatformHome';
 
 class Platform extends Component {
   render() {
     return (
       <div className="Platform">
-        <button className="platformlinks-chat platformlinks">
-          <Link to="/platform/chat"> Chatroom </Link>
-        </button>
+        <nav>
+          <button className="platformlinks-chat platformlinks">
+            <Link to="/platform/home"> Home </Link>
+          </button>
+          <button className="platformlinks-chat platformlinks">
+            <Link to="/platform/chat"> Chatroom </Link>
+          </button>
+        </nav>
+        <Route path="/platform/home" component={ PlatformHome } />
         <Switch>
           <Route path="/platform/chat" component={ SocketChat } />
         </Switch>
