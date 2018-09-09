006b11d8eecdfb9afeca1ec738ae822743b05df5
Jesse Ma <j.masonoma17@gmail.com>
2018-09-08 23:46:52 -0700

    Initialize oAuth for google sign in in componentDidMount() at Login component

--- nbbm-app/frontend/src/components/Login.js
+++ nbbm-app/frontend/src/components/Login.js
@@ -49,12 +49,32 @@ class Login extends Component {
     })
   }
 
+  onGoogleSignIn(success) {
+    console.log(success);
+  }
+
+  onGoogleSignInFailure(error) {
+    console.log(error);
+  }
+
+  componentDidMount() {
+    window.gapi.load('auth2', () => {
+      let auth2 = window.gapi.auth2.init({
+        client_id: "639843179540-b5t5jbeng5l0hoa97p4bjaj04bsburvr.apps.googleusercontent.com",
+        fetch_basic_profile: true,
+        scope: "profile email"
+      });
+      auth2.then(this.onGoogleSignIn, this.onGoogleSignInFailure)
+    })
+  }
+
   render() {
     return (
       <div className="Login">
         <button className="back-button">
           <Link exact="true" to="/"><FontAwesomeIcon className="angle-left-icon" icon="angle-left" size="2x" /></Link>
         </button>
+        <div className="g-signin2" data-onsucess={this.onSignIn} />
         <h1 className="app-title"> North Bay Badminton Group </h1>
         <div className="login-container">
           <form className="login-form" onSubmit={(e) => this.handleLogin(e)}>
