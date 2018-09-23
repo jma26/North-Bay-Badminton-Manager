7a4aca0f8e9c8e40d8bfc0e5affc29d3d939a88a
Jesse Ma <j.masonoma17@gmail.com>
2018-09-22 22:56:56 -0700

    Add new component, JoinLeague

---
+++ nbbm-app/frontend/src/components/JoinLeague.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './JoinLeague.css';
+
+class JoinLeague extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="JoinLeague">
+        <p> Hello </p>
+      </div>
+    )
+  }
+}
+
+export default JoinLeague;
