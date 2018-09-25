42cbac669e3cdc02f520ddf9b85cd07bc69233dd
Jesse Ma <j.masonoma17@gmail.com>
2018-09-24 21:38:30 -0700

    Update JoinLeague component to call /getAllLeagues to retrieve all leagues from mysql db

--- nbbm-app/frontend/src/components/JoinLeague.js
+++ nbbm-app/frontend/src/components/JoinLeague.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import './JoinLeague.css';
 
+import axios from 'axios';
+
 class JoinLeague extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      'leagues': []
+    }
   }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000/getAllLeagues')
+    .then((res) => {
+      console.log(res);
+    })
+    .then((leagues) => {
+      this.setState({
+        'leagues': [...leagues]
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
   render() {
+    var no_leagues;
+    // Check if any leagues exist
+    if (this.state.leagues.length < 1) {
+      no_leagues = <p> No available leagues </p>
+    }
     return (
       <div className="JoinLeague">
-        <p> Hello </p>
+        <p> {no_leagues} </p>
       </div>
     )
   }
