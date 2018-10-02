d6f63e27a1adae1d7e2b068c7a4923af4b046ebc
Jesse Ma <j.masonoma17@gmail.com>
2018-10-01 17:30:46 -0700

    Update ChatMessages.js to a class component, and scroll to bottom when new message appears

--- nbbm-app/frontend/src/components/ChatMessages.js
+++ nbbm-app/frontend/src/components/ChatMessages.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ChatMessage.css';
 
-function ChatMessages(props) {
-  
-  return (
-    <div className="ChatMessages">
-      <ol>
-    {
-      props.chatHistory.map((message, i) => 
-        <li key={i} className="message"> {message} </li>
-      )
-    }
-      </ol>
-    </div>
-  )
+class ChatMessages extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  scrollToBottom() {
+    this.messageEnd.scrollIntoView({ behavior: 'smooth'});
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  render() {
+    return (
+      <div className="ChatMessages">
+        <ol>
+      {
+        this.props.chatHistory.map((message, i) => 
+          <li key={i} className="message"> {message} </li>
+        )
+      }
+        </ol>
+        <div style={{float: 'left', clear: 'both'}} 
+              ref={(el) => {this.messageEnd = el;}}> 
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ChatMessages;
