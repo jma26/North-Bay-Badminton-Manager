e21e1ae7c96482ce7817e15d4d2c310da7298922
Jesse Ma <j.masonoma17@gmail.com>
2018-10-02 22:10:39 -0700

    Update joinleague() and handleLeagueRegistration() to check if user is captain of league and whether there is another successor for captain

--- nbbm-app/frontend/src/components/JoinLeague.js
+++ nbbm-app/frontend/src/components/JoinLeague.js
@@ -84,39 +84,76 @@ class JoinLeague extends Component {
     let league = this.state.league_name
     // Save league name to fireStore
     fireStore.collection('leagues').doc(league).set({
-      'roster': [this.props.location.state.name]
+      'roster': [this.props.location.state.name],
+      'captain': this.props.location.state.name
     })
     .then(() => {
       console.log("Document successfully written");
-    })
-    // Update user's league affiliation 
-    .then(() => {
-      console.log(this.props.location.state.name);
-      fireStore.collection('users').doc(this.props.location.state.name)
-      .update({
-        league: league
+      // Retrieve index from old league roster
+      let index = this.state.my_teammates.indexOf(this.props.location.state.name);
+      // Remove user from old league roster
+      let my_old_teammates = this.state.my_teammates;
+      my_old_teammates.splice(index, 1);
+      console.log(my_old_teammates);
+      // Delete user from old league roster in firestore
+      fireStore.collection("leagues").doc(this.state.my_league).update({
+        roster: my_old_teammates,
       })
-      .then(() => {
-        console.log("Document successfully updated");
-        // Reset league_name state
-        this.setState({
-          league_name: ''
-        })
-        // Re-render league info, call renderUsersTeam and getAllLeagues
-        this.renderUsersTeam();
-        this.getAllLeagues();
-      })
-      .catch((error) => {
-        console.log("Error updating document: ", error);
+      // Check if user is captain of old league roster
+      fireStore.collection('leagues').doc(this.state.my_league).get()
+      .then((doc) => {
+        let roster = doc.data().roster
+        console.log('Captain of the leageu is: ', doc.data().captain);
+        // Relinquish user of captain status of old league
+        if (doc.data().captain === this.props.location.state.name) {
+          fireStore.collection('leagues').doc(this.state.my_league).update({
+            captain: ''
+          })
+        }
+        // Delete league if no one is in the league
+        if (roster.length <= 0) {
+          fireStore.collection('leagues').doc(this.state.my_league).delete()
+          .then((doc) => {
+            console.log('Document successfully deleted');
+          })
+          .catch((error) => {
+            console.log('Error removing document: ', error);
+          })
+        } else if (roster.length > 0) {
+          fireStore.collection('leagues').doc(this.state.my_league).update({
+            captain: roster[0]
+          })
+        }
       })
     })
     .catch((error) => {
-      console.log("Error writing document: ", error);
+      console.log('Error creating document: ', error);
+    })
+    // Update user's league affiliation 
+    fireStore.collection('users').doc(this.props.location.state.name)
+    .update({
+      league: league
+    })
+    .then(() => {
+      console.log("Document successfully updated");
+      // Reset league_name state
+      this.setState({
+        league_name: ''
+      })
+      // Re-render league info, call renderUsersTeam and getAllLeagues
+      this.renderUsersTeam();
+      this.getAllLeagues();
+    })
+    .catch((error) => {
+      console.log('Error updating document: ', error);
     })
   }
   
   joinleague(league) {
-    if (!this.state.my_league || !this.state.my_teammates) {
+    // Check if joining the same league
+    if (this.state.my_league === league) {
+      console.log('You are already in your league');
+    } else if (!this.state.my_league || !this.state.my_teammates) {
       // Update user's league affiliation
       fireStore.collection("users").doc(this.props.location.state.name).update({
         league: league
@@ -133,6 +170,21 @@ class JoinLeague extends Component {
       })
       .catch((error) => {
         console.log("Error updating document: ", error);
+      })
+      // Update new league roster with user
+      fireStore.collection("leagues").doc(league).get()
+      .then((doc) => {
+        // League roster from new team
+        var roster = doc.data().roster;
+        // Add user to new league roster
+        roster.push(this.props.location.state.name);
+        // Update new league roster in firestore
+        fireStore.collection("leagues").doc(league).update({
+          roster: roster
+        })
+      })
+      .catch((error) => {
+        console.log('Error updating document: ', error);
       })
     } else {
       console.log(this.state.my_teammates);
@@ -141,10 +193,36 @@ class JoinLeague extends Component {
       // Remove user from old league roster
       let my_old_teammates = this.state.my_teammates;
       my_old_teammates.splice(index, 1);
-      console.log(index, my_old_teammates);
+      console.log(my_old_teammates);
       // Delete user from old league roster in firestore
       fireStore.collection("leagues").doc(this.state.my_league).update({
-        roster: my_old_teammates
+        roster: my_old_teammates,
+      })
+      // Check if user is captain of old league roster
+      fireStore.collection('leagues').doc(this.state.my_league).get()
+      .then((doc) => {
+        let roster = doc.data().roster
+        console.log('Captain of the leageu is: ', doc.data().captain);
+        // Relinquish user of captain status of old league
+        if (doc.data().captain === this.props.location.state.name) {
+          fireStore.collection('leagues').doc(this.state.my_league).update({
+            captain: ''
+          })
+        }
+        // Delete league if no one is in the league
+        if (roster.length <= 0) {
+          fireStore.collection('leagues').doc(this.state.my_league).delete()
+          .then((doc) => {
+            console.log('Document successfully deleted');
+          })
+          .catch((error) => {
+            console.log('Error removing document: ', error);
+          })
+        } else if (roster.length > 0) {
+          fireStore.collection('leagues').doc(this.state.my_league).update({
+            captain: roster[0]
+          })
+        }
       })
       // Update user's league affiliation
       fireStore.collection("users").doc(this.props.location.state.name).update({
@@ -163,19 +241,19 @@ class JoinLeague extends Component {
       .catch((error) => {
         console.log("Error updating document: ", error);
       })
-    }
-    // Update new league roster with user
-    fireStore.collection("leagues").doc(league).get()
-    .then((doc) => {
-      // League roster from new team
-      var roster = doc.data().roster;
-      // Add user to new league roster
-      roster.push(this.props.location.state.name);
-      // Update new league roster in firestore
-      fireStore.collection("leagues").doc(league).update({
-        roster: roster
+      // Update new league roster with user
+      fireStore.collection("leagues").doc(league).get()
+      .then((doc) => {
+        // League roster from new team
+        var roster = doc.data().roster;
+        // Add user to new league roster
+        roster.push(this.props.location.state.name);
+        // Update new league roster in firestore
+        fireStore.collection("leagues").doc(league).update({
+          roster: roster
+        })
       })
-    })
+    }
   }
   
   render() {
