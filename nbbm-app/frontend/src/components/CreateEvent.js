0ff4e4223d381bad9ecb7f265ce20182796590c7
Jesse Ma <j.masonoma17@gmail.com>
2018-10-04 23:07:55 -0700

    CreateEvent component: Add FontAwesomeIcon icons to form

--- nbbm-app/frontend/src/components/CreateEvent.js
+++ nbbm-app/frontend/src/components/CreateEvent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './CreateEvent.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 class CreateEvent extends Component {
   constructor(props) {
     super(props);
@@ -30,24 +32,27 @@ class CreateEvent extends Component {
       <div className="CreateEvent">
         <form className="new-event-form" onSubmit={(e) => this.submitNewEvent(e)}>
           <div>
-            <label className={['title', 'input-field'].join(' ')}>
+            <label className="title"> Name of event
               <input type="text" name="title" placeholder="Title of event" value={this.state.title} autoComplete="off" onChange={(e) => this.handleChange(e)}></input>
             </label>
           </div>
           <div>
-            <label className={['allDay', 'radio-field']}> All day? </label>
-            <input type="radio" name="allDay" value="true" onChange={(e) => this.handleChange(e)}></input>
-            <input type="radio" name="allDay" value="false" onChange={(e) => this.handleChange(e)}></input>
+            <label className="allDay"> All day? 
+              <input type="radio" name="allDay" value="true" onChange={(e) => this.handleChange(e)}></input>
+              <input type="radio" name="allDay" value="false" onChange={(e) => this.handleChange(e)}></input>
+            </label>
           </div>
           <div>
-            <label className={['start', 'input-field'].join(' ')}>
+            <label className="start">
+              <FontAwesomeIcon className="start" icon="hourglass-start" />
               <input type="date" name="start" onChange={(e) => this.handleChange(e)} required></input>
               <input type="time" name="startTime" min="7:00" max="20:00" onChange={(e) => this.handleChange(e)}
               required></input>
             </label>
           </div>
           <div>
-            <label className={['end', 'input-field'].join(' ')}>
+            <label className="end">
+            <FontAwesomeIcon className="end" icon="hourglass-end" />
               <input type="date" name="end" onChange={(e) => this.handleChange(e)} required></input>
               <input type="time" name="endTime" min="7:00" max="24:00" onChange={(e) => this.handleChange(e)} required></input>
             </label>
