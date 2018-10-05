6eae13c6bd5fbda4f0a1fa0757ebce87d30b6024
Jesse Ma <j.masonoma17@gmail.com>
2018-10-04 21:14:00 -0700

    CreateEvent component: Initial new component

---
+++ nbbm-app/frontend/src/components/CreateEvent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './CreateEvent.css';
+
+class CreateEvent extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div className="CreateEvent">
+        <p> Test </p>
+      </div>
+    )
+  }
+}
+
+export default CreateEvent;
