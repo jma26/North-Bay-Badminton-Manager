ac455bf16d964d1f08c2c971bf3f4b39dc60e8df
Jesse Ma <j.masonoma17@gmail.com>
2018-10-04 21:39:34 -0700

    CreateEvent component: Add form elements and state for input changes

--- nbbm-app/frontend/src/components/CreateEvent.js
+++ nbbm-app/frontend/src/components/CreateEvent.js
@@ -4,11 +4,55 @@ import './CreateEvent.css';
 class CreateEvent extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      title: '',
+      allDay: false,
+      start: '',
+      end: '',
+      startTime: '',
+      endTime: ''
+    }
   }
+
+  submitNewEvent(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     return(
       <div className="CreateEvent">
-        <p> Test </p>
+        <form className="new-event-form" onSubmit={(e) => this.submitNewEvent(e)}>
+          <div>
+            <label className={['title', 'input-field'].join(' ')}>
+              <input type="text" name="title" placeholder="Title of event" value={this.state.title} autoComplete="off" onChange={(e) => this.handleChange(e)}></input>
+            </label>
+          </div>
+          <div>
+            <label className={['allDay', 'radio-field']}> All day? </label>
+            <input type="radio" name="allDay" value="true" onChange={(e) => this.handleChange(e)}></input>
+            <input type="radio" name="allDay" value="false" onChange={(e) => this.handleChange(e)}></input>
+          </div>
+          <div>
+            <label className={['start', 'input-field'].join(' ')}>
+              <input type="date" name="start" onChange={(e) => this.handleChange(e)} required></input>
+              <input type="time" name="startTime" min="7:00" max="20:00" onChange={(e) => this.handleChange(e)}
+              required></input>
+            </label>
+          </div>
+          <div>
+            <label className={['end', 'input-field'].join(' ')}>
+              <input type="date" name="end" onChange={(e) => this.handleChange(e)} required></input>
+              <input type="time" name="endTime" min="7:00" max="24:00" onChange={(e) => this.handleChange(e)} required></input>
+            </label>
+          </div>
+        </form>
       </div>
     )
   }
